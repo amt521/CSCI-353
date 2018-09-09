e7e1eb48417865cd2882c0cc75cedc2eca14eec9
Armando Tan <tanarman@usc.edu>
2018-09-09 10:02:48 -0700

    Assignment 1 v4

--- client.py
+++ client.py
@@ -2,30 +2,30 @@ import socket
 import sys
  
 # Create a TCP/IP socket
-stream_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
  
-# Define host
+# Define variables
 host = 'localhost'
- 
 # define the communication port
 portNum = raw_input("Enter Port Number: ")
 print("You entered: ", portNum)
+
+message = raw_input("Enter Message: ")
  
 # Connect the socket to the port where the server is listening
 server_address = ((host, portNum))
  
 print("connecting")
  
-stream_socket.connect(server_address)
+client_socket.connect(server_address)
 print("client1# connected to server and registered")
  
 # Send data
-message = 'message'
-stream_socket.sendall(message)
+client_socket.sendall(message)
  
 # response
-data = stream_socket.recv(10)
+data = client_socket.recv(10)
 print(data) 
  
 print('socket closed')
-stream_socket.close()
+client_socket.close()
