5bbdb699d7006c3ee96588a15fbad0f9a262f9e2
Armando Tan <tanarman@usc.edu>
2018-09-10 20:16:11 -0700

    Assignment1 v9

--- client.py
+++ client.py
@@ -5,20 +5,20 @@ argv=sys.argv[1:len(sys.argv)]
 ipaddress = str(argv[1])
 portnum = int(argv[3])
 logfile = str(argv[5])
-myname = argv[7]
-
+myname = str(argv[7])
 if (argv == None):
     print("Input information in the form: client.py –s serverIP –p portno –l logfile –n myname")
     sys.exit()
 
 #process logfile
-outputFile = open(logfile, "w")
+outputFile = open(logfile, "w+")
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 except socket.error:
-    print 'Failed to create socket'
+    print '<DEBUG> Failed to create socket'
     outputFile.write("terminating client...")
+    print (myname + " exit")
     outputFile.close()
     sys.exit()
  
@@ -26,27 +26,31 @@ host = ipaddress
  
 continue_check = True 
 while(continue_check) :
-    msg = raw_input('Enter message to send : ')
-     
+    msg = raw_input('Enter message to send: ')
+    dest = raw_input('Enter who to send to: ') 
     try :
         if (msg == 'exit'):
             outputFile.write("terminating client...")
+            print (myname + " exit")
             outputFile.close()
             sys.exit()
 
-        #Set the whole string
-        s.sendto(msg, (host, portnum))
+        #Set the whole string & encode the string
+        s.sendto(msg, myname.encode('utf-8'), dest.encode('utf-8'), (host, portnum))
+        print(myname + " send to " + dest + " " + msg)
         outputFile.write("connecting to the server " + host + " at port " + portnum)
          
         # receive data from client (data, addr)
         d = s.recvfrom(1024)
         reply = d[0]
         addr = d[1]
-         
-        print 'Server reply : ' + reply
+
+        print(myname + " connected to server and registered") 
+        print '<DEBUG> Server reply : ' + reply
      
     except socket.error, msg:
-        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        print '<DEBUG> Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        print (myname + " exit")
         outputFile.write("terminating client...")
         outputFile.close()
         sys.exit()
--- server.py
+++ server.py
@@ -6,52 +6,52 @@ HOST = ''   # Symbolic name meaning all available interfaces
 argv=sys.argv[1:len(sys.argv)]
 portnum = int(argv[1])
 logfile = str(argv[3])
-handnum = argv[5]
+handnum = int(argv[5])
 
 #process logfile
-outputFile = open(logfile, "w")
-
-if (argv == None):
-    print("Input information in the form: server.py –p portno –l logfile -h handler")
-    outputFile.write("terminating server...")
-    outputFile.close()
-    sys.exit()
+outputFile = open(logfile, "w+")
 
 # Datagram (udp) socket
 try :
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print 'Socket created'
 except socket.error, msg :
-    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print '<DEBUG> Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     outputFile.write("terminating server...")
     outputFile.close()
     sys.exit()
- 
  
 # Bind socket to local host and port
 try:
     s.bind((HOST, portnum))
+
+    if (argv[1] == ""):
+        print("Input information in the form: server.py –p portno –l logfile -h handler")
+        outputFile.write("terminating server...")
+        outputFile.close()
+        sys.exit()
 except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print '<DEBUG> Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     outputFile.write("terminating server...")
     outputFile.close()
     sys.exit()
-     
-print 'Socket bind complete'
  
 #now keep talking with the client
 while 1:
     # receive data from client (data, addr)
     d = s.recvfrom(1024)
     data = d[0]
-    addr = d[1]
+    sender = d[1].decode('utf8')
+    dest = d[2].decode('utf8')
+    addr = d[3]
      
     if not data: 
         break
-     
-    reply = 'OK...' + data
+
+    outputFile.write(sender + " registered from host") 
+    reply = '<DEBUG> OK...' + data
      
     s.sendto(reply , addr)
-    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+    print '<DEBUG> Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
      
 s.close()
